b67b73be2cdf220d1970420896ebdbc38ead5866
Veeti Poutsalo <veeti@poutsalo.com>
2021-01-27 18:45:09 +0200

    Create initial Express app

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const PORT = 8000
+
+const app = express()
+
+app.get('/', (_req, res) => {
+	res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
+})
